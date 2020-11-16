d0184dc4ca172d2b329a4b61752911e4f642ba75
M Shibly <shibly.phy@gmail.com>
2020-11-16 18:19:10 +0600

    added TaskController and TaskRepository

---
+++ app/Repositories/TaskRepository.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace App\Repositories;
+
+
+use App\Interfaces\CrudInterface;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskRepository implements CrudInterface
+{
+
+
+    public function getAll()
+    {
+        $tasks = Task::all();
+        return $tasks;
+    }
+
+    public function findById($id)
+    {
+        $task = Task::with('project')
+            ->find($id);
+        return $task;
+    }
+
+    public function create(Request $request)
+    {
+        $task = new Task();
+        $task->name = $request->name;
+        $task->description = $request->description;
+        $task->project_id = $request->project_id;
+        $task->save();
+        return $task;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $task = $this->findById($id);
+        $task->name = $request->name;
+        $task->description = $request->description;
+        $task->project_id = $request->project_id;
+        $task->save();
+        return $task;
+    }
+
+    public function delete($id)
+    {
+        $task = $this->findById($id);
+        $task->delete();
+        return $task;
+    }
+
+
+}
